0b2f61d359e139acf78ca2c0f0fb585e78a0160c
leandro-driguez <leandro__rodriguez@outlook.com>
2022-09-06 17:36:59 -0400

    feat(api): add ´Wallet´, ´Payment´ and ´Utility tokens´ urls of the API

--- fusyona/constants/url.py
+++ fusyona/constants/url.py
@@ -37,3 +37,96 @@ PaymentCancel: Callable[[str], str] = lambda id : f"https://api.fusyona.com/nfts
 
 PaymentConfirmation: Callable[[str], str] = lambda id : f"https://api.fusyona.com/nfts/v1/PaymentConfirmation/{id}"
 """POST: PaymentConfirmation"""
+
+#########################################
+################ Wallet #################  
+#########################################  
+
+BalanceAsync : Callable [[str], str] = lambda address_id : f"https://api.fusyona.com/wallet/v1/addresses/{address_id}/balance" 
+"""GET : BalanceAsync"""
+
+FullBalanceAsync : Callable [[str], str] = lambda address_id : f"https://api.fusyona.com/wallet/v1/addresses/{address_id}/fullbalance" 
+"""GET : FullBalanceAsync"""
+
+ClientAddressesAndBalanceAsync : Callable [[], str] = lambda : f"https://api.fusyona.com/wallet/v1/fulladdresses" 
+"""GET : GetClientAddressesAndBalanceAsync"""
+
+CryptocurrenciesAsync : Callable [[], str] = lambda : f"https://api.fusyona.com/wallet/v1/cryptocurrencies" 
+"""GET : CryptocurrenciesAsync"""
+
+CryptocurrencySubUnistAsync: Callable [[str], str] = lambda cryptocurrency_id : f"https://api.fusyona.com/wallet/v1/cryptocurrencies/{cryptocurrency_id}/subunits" 
+"""GET : CryptocurrencySubUnistAsync"""
+
+WithdrawalAsync :  Callable [[str, str, str], str] = lambda address_id, to_public_key, amount :  f"https://api.fusyona.com/wallet/v1/addresses/{address_id}/transfer/{to_public_key}/{amount}" 
+"""POST: WithdrawalAsync"""
+
+#########################################
+################ Payment ################  
+#########################################  
+
+Checkout :  Callable [[str, str, str], str] = lambda address_id, to_public_key, amount :  f"https://api.fusyona.com/pay/v1/Checkout" 
+"""POST: Checkout"""
+
+CheckoutCancel :  Callable [[str], str] = lambda id :  f"https://api.fusyona.com/pay/v1/CheckoutCancel/{id}" 
+"""POST: CheckoutCancel"""
+
+CheckoutConfirmation :  Callable [[str], str] = lambda id :  f"https://api.fusyona.com/pay/v1/CheckoutConfirmation/{id}" 
+"""POST: CheckoutConfirmation"""
+
+ClaimGift :  Callable [[str], str] = lambda tokenId :  f"https://api.fusyona.com/pay/v1/{tokenId}/claim" 
+"""POST: ClaimGift"""
+
+Invoices : Callable [[], str] = lambda : f"https://api.fusyona.com/pay/v1/Invoices" 
+"""GET : Invoices"""
+
+Orders : Callable [[], str] = lambda : f"https://api.fusyona.com/pay/v1/Orders" 
+"""GET : Orders"""
+
+#########################################
+############ Utility Tokens #############  
+#########################################
+
+CreateERC20 :  Callable [[], str] = lambda :  f"https://api.fusyona.com/utility/v1/erc20" 
+"""POST: CreateERC20"""
+
+
+
+#########################################
+################ Social #################  
+#########################################
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
